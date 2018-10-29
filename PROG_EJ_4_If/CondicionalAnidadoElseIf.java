a1569b0347732b8ba2eb7a64f7b2d202ec520991
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2018-10-29 12:06:02 +0100

    Reupload Prog EJ 4 IF

---
+++ PROG_EJ_4_If/CondicionalAnidadoElseIf.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PROG_EJ_4_If;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fsancheztemprano
+ */
+public class CondicionalAnidadoElseIf {
+
+    public void validarConAnidadosElseIf() {
+        int num1, num2;
+        String numero1 = JOptionPane.showInputDialog("Teclea el numero 1: ");
+        num1 = Integer.parseInt(numero1);
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Teclea el numero 2: "));
+
+        if (num1 > num2) {
+            System.out.println(num1 + "es mayor");
+        } else if (num2 > num1) {
+            System.out.println(num2 + "es mayor");
+        } else {
+            System.out.println("son iguales");
+
+        }
+
+    }
+
+}
---
+++ PROG_EJ_4_If/PROG_EJ_4_If.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PROG_EJ_4_If;
+
+//import java.util.Scanner;
+/**
+ *
+ * @author fsancheztemprano
+ */
+public class PROG_EJ_4_If {
+
+    public static void main(String[] args) {
+        CondicionalSimple obxCS = new CondicionalSimple();
+        //   obxCS.menorEdad();
+        CondicionalDoble obxCD = new CondicionalDoble();
+        // obxCD.mayorMenorEdad();
+
+        CondicionalAnidado obxCA = new CondicionalAnidado();
+       // obxCA.validarConAnidados();
+
+        CondicionalAnidadoElseIf obxCAei = new CondicionalAnidadoElseIf();
+        obxCAei.validarConAnidadosElseIf();
+
+    }
+}
