d3cbf7047431e5efb8e70b375f5b1ad6e0c5af48
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2018-11-07 11:51:37 +0100

    Boletin Extras 2 Calc Nota Media de un grupo de estudiantes

---
+++ PROG_Extras_2_CalcNota/PROG_Extras_2_CalcNota.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PROG_Extras_2_CalcNota;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fsancheztemprano
+ */
+public class PROG_Extras_2_CalcNota {
+    public static void main(String[] args) {
+        float acumuladorNotas=0f;
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Introduce cuantos estudiantes:");
+        int cantidadAlumnos = scan.nextInt();
+        
+        for(int i=1;i <= cantidadAlumnos;i++){
+            System.out.println("Estudiante numero: " + i);
+            Estudiante estu = new Estudiante();
+            estu.pedirDatos();
+            acumuladorNotas += estu.calcNotaFinal();
+    
+        }
+        float notaMediaGrupo = Estudiante.calcNotaMediaGrupo(acumuladorNotas, cantidadAlumnos);
+        System.out.println("Nota Media Grupo: "+notaMediaGrupo);
+        
+    
+
+
+}
+
+    
+}
