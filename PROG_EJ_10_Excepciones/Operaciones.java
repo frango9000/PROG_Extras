bfa6167d851236d978c5f6f1b4c65a32c630542e
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2018-12-10 10:59:13 +0100

    excepciones

--- PROG_EJ_10_Excepciones/Operaciones.java
+++ PROG_EJ_10_Excepciones/Operaciones.java
@@ -1,22 +1,39 @@
 package PROG_EJ_10_Excepciones;
 
 public class Operaciones {
-    public void suma(int i, int j){
-        System.out.println("Suma numero: "+i+" + "+j+" = "+ (i+j));
+
+    public void suma(int i, int j) {
+        System.out.println("Suma numero: " + i + " + " + j + " = " + (i + j));
     }
-    public void resta(int i, int j){
-        System.out.println("Resta numero: "+i+" - "+j+" = "+ (i-j));
+
+    public void resta(int i, int j) {
+        System.out.println("Resta numero: " + i + " - " + j + " = " + (i - j));
     }
-    public void multi(int i, int j){
-        System.out.println("Multiplicacion numero: "+i+" x "+j+" = "+ (i*j));
+
+    public void multi(int i, int j) {
+        System.out.println("Multiplicacion numero: " + i + " x " + j + " = " + (i * j));
     }
-    public void divis(int i, int j){
-        System.out.println("Division numero: "+i+" / "+j+" = "+ (i/j));
+
+    public void divisExc(int i, int j) {
+        if (j == 0) {
+            System.out.println("Error");
+        } else {
+            System.out.println("Division numero: " + i + " / " + j + " = " + (i / j));
+        }
     }
-    public void all(int i, int j){
-        suma(i,j);
-        resta(i,j);
-        multi(i,j);
-        divis(i,j);
+
+    public void divis(int i, int j) {
+        System.out.println("Division numero: " + i + " / " + j + " = " + (i / j));
+    }
+
+    public void all(int i, int j) {
+        suma(i, j);
+        resta(i, j);
+        multi(i, j);
+        try {
+            divis(i, j);
+        } catch (ArithmeticException e) {
+            System.out.println("Error al dividir entre 0");
+        }
     }
 }
--- PROG_EJ_10_Excepciones/PROG_EJ_Excepciones.java
+++ PROG_EJ_10_Excepciones/PROG_EJ_Excepciones.java
@@ -1,26 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PROG_EJ_10_Excepciones;
 
-/**
- *
- * @author fsancheztemprano
- */
 public class PROG_EJ_Excepciones {
+
     public static void main(String[] args) {
+
         Operaciones op = new Operaciones();
-//        op.suma(8, 2);
-//        op.resta(3, 2);
-//        op.multi(5, 3);
-//        op.divis(8, 2);
-        try{
-            op.all(8, 0);
-        }catch(ArithmeticException e){
-            System.out.println("Error al divideir entre 0");
-        }
+
+        op.all(8, 0);
     }
-    
 }
