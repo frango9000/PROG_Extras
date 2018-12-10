edf02bd0459ce950d0c5dda324024acc1186cc0c
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2018-12-10 10:29:35 +0100

    init EJ 10 Excepciones

---
+++ PROG_EJ_10_Excepciones/PROG_EJ_Excepciones.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PROG_EJ_10_Excepciones;
+
+/**
+ *
+ * @author fsancheztemprano
+ */
+public class PROG_EJ_Excepciones {
+    public static void main(String[] args) {
+        
+    }
+    
+}
