4b910adb846cf538312755ce94d022da30cfada9
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-01-28 12:38:58 +0100

    13

---
+++ PROG_Ej_13_Array2D/App.java
@@ -0,0 +1,9 @@
+package PROG_Ej_13_Array2D;
+//@author fsancheztemprano
+public class App {
+public static void main(String[] args){
+    Notas notas = new Notas();
+    notas.mostrar();
+    notas.mostrarMediaModulo();
+}
+}
