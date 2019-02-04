9ea78bb196ed7d1850807a9837710a585b99978b
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-02-04 10:45:17 +0100

    bin

---
+++ test/toBin.java
@@ -0,0 +1,18 @@
+package test;
+
+//@author fsancheztemprano
+public class toBin {
+    public static void main(String[] args){
+        System.out.println(todec("1111111"));
+        System.out.println(tobin(254));
+        
+    }
+    
+    public static String tobin(int i){
+        return Integer.toBinaryString(i);
+    }
+    public static int todec(String i){
+        return Integer.parseInt(i, 2);
+    }
+
+}
