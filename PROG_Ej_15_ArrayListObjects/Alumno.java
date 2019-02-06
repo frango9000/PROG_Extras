47c13a9f893c9d470e58d7547a6b3ea179191b20
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-02-06 10:45:29 +0100

    Ej_15

---
+++ PROG_Ej_15_ArrayListObjects/Alumno.java
@@ -0,0 +1,47 @@
+package PROG_Ej_15_ArrayListObjects;
+//@author fsancheztemprano
+public class Alumno implements Comparable {
+    String nombre;
+    String dni;
+    int nota;
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre, String dni, int nota) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.nota = nota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+
+}
---
+++ PROG_Ej_15_ArrayListObjects/Main.java
@@ -0,0 +1,14 @@
+package PROG_Ej_15_ArrayListObjects;
+//@author fsancheztemprano
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+        ArrayList alumnos;
+        alumnos = new ArrayList<Alumno>();
+        
+        
+    }
+
+}
