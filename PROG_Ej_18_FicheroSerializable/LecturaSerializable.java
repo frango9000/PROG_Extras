f180ec90cbd744ee55d3de023310db3bf82badd2
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-02-27 11:29:14 +0100

    alt

--- PROG_Ej_18_FicheroSerializable/LecturaSerializable.java
+++ PROG_Ej_18_FicheroSerializable/LecturaSerializable.java
@@ -17,16 +17,18 @@ public class LecturaSerializable {
     public ArrayList<Alumno> leerObjeto(String filename){
         ArrayList<Alumno> alumnos = new ArrayList<>();
         Alumno al = null;
+        boolean x = true;
         try{
             file = new FileInputStream(filename + ".dat");
             read = new ObjectInputStream(file);
             
-            while (true){
+            while (x){
                 if (file.available() != 0){
                     al = (Alumno) read.readObject(); 
                     alumnos.add(al);
                     System.out.println(al);
-                }else break;
+                }else 
+                    x = false;
             }
             System.out.println(alumnos);
             
