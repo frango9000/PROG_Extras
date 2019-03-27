4955064c26c86810630a6e905807fc242b6ca822
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-03-27 11:38:24 +0100

    Swing Eventos

--- PROG_Ej_20_Eventos/NewMain.java
+++ PROG_Ej_20_Eventos/NewMain.java
@@ -1,12 +1,27 @@
 package PROG_Ej_20_Eventos;
+
 /**
  * @author fsancheztemprano
  */
 public class NewMain {
-    public static void main(String[] args) {
-        
-            Eventos e = new Eventos();
-            e.crearVentana();
 
+    public static void main(String[] args) {
+        e3();
+
+    }
+
+    static void e1() {
+        Eventos e = new Eventos();
+        e.crearVentana();
+    }
+
+    static void e2() {
+        EventosInner e = new EventosInner();
+        e.crearVentana();
+    }
+
+    static void e3() {
+        EventosAnon e = new EventosAnon();
+        e.crearVentana();
     }
 }
