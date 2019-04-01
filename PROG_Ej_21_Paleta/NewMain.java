09065f9c9b71d0c432edbdedde43dad7bfa8f83b
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-04-01 11:54:14 +0200

    gui con paleta

---
+++ PROG_Ej_21_Paleta/NewMain.java
@@ -0,0 +1,12 @@
+package PROG_Ej_21_Paleta;
+/**
+ * @author fsancheztemprano
+ */
+public class NewMain {
+    public static void main(String[] args) {
+        
+        VentanaP f = new VentanaP();
+        f.setVisible(true);
+
+    }
+}
