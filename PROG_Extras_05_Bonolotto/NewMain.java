6b48351c433292cacacc564ff48f465aa20afbed
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-04-09 12:39:08 +0200

    bonolotto

---
+++ PROG_Extras_05_Bonolotto/NewMain.java
@@ -0,0 +1,12 @@
+package PROG_Extras_05_Bonolotto;
+/**
+ * @author fsancheztemprano
+ */
+public class NewMain {
+    public static void main(String[] args) {
+        Loteria loteria = new Loteria();
+        loteria.setVisible(true);
+
+
+    }
+}
